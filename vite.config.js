ceb2a845bd2d062e85a6a0c8792abba6ef0860e9
yoga <marian.leonor.perea@gmail.com>
2026-07-19 21:09:39 -0300

    Agregar Dockerfile optimizado con multi-stage build

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,14 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-export default defineConfig({
+export default defineConfig(({ mode }) => ({
 
     plugins: [
         react()
     ],
 
-    base: "/ReactJsCoder/"
+    base: process.env.GITHUB_PAGES
+    ? "/ReactJsCoder/"
+    : "/"
 
-});
+}));
